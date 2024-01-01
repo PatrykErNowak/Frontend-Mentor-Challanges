847f2720e789563e6918be729d7d11dbb7cd9001
Patryk Nowak <patryk.ernest.nowak@gmail.com>
2024-01-01 22:36:06 +0100

    Interactive rating component - Implement change state by JS

--- Challenges/01 Newbie/interactive-rating-component/src/js/main.js
+++ Challenges/01 Newbie/interactive-rating-component/src/js/main.js
@@ -1,1 +1,35 @@
-console.log("Script loaded");
+'use strict';
+
+const state = {
+  init: {
+    container: document.querySelector('.js--rating-form'),
+    submitBtn: document.querySelector('.js--rating-btn'),
+    inputRadios: document.querySelectorAll('.js--rating-radio'),
+  },
+
+  end: {
+    container: document.querySelector('.js--rating-thankyou'),
+    userRating: document.querySelector('.js--rating-userrating'),
+  },
+};
+
+const classToggle = function (element, ...classes) {
+  classes.forEach((c) => element.classList.toggle(c));
+};
+
+state.init.submitBtn.addEventListener('click', (e) => {
+  let rating;
+  const { init, end } = state;
+
+  e.preventDefault();
+  init.inputRadios.forEach((radio) => {
+    if (radio.checked) rating = radio.value;
+  });
+
+  end.userRating.textContent = rating;
+
+  if (rating) {
+    classToggle(init.container, 'hidden', 'flex');
+    classToggle(end.container, 'hidden', 'flex');
+  }
+});
