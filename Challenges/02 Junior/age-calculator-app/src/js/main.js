82a9a28675b164c88cf810a7bd2da833ddd3d17a
Patryk Nowak <patryk.ernest.nowak@gmail.com>
2024-01-31 14:17:36 +0100

    Age Calculator - Make design & layout for desktop and mobile, without active states and errors handling

---
+++ Challenges/02 Junior/age-calculator-app/src/js/main.js
@@ -0,0 +1,9 @@
+console.log('Script loaded');
+
+const inputDay = document.querySelector('#form-day');
+const inputMonth = document.querySelector('#form-month');
+const inputYear = document.querySelector('#form-year');
+
+// inputDay.value = 24;
+// inputMonth.value = 12;
+// inputYear.value = 1987;
