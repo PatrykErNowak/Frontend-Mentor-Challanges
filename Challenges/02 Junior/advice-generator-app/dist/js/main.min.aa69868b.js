0502f79ba83b4541ef758e81ffb6b5d279d9e4d6
Patryk Nowak <patryk.ernest.nowak@gmail.com>
2024-02-05 14:47:43 +0100

    Advice Generator - Implement fetch data and render data in App

--- Challenges/02 Junior/advice-generator-app/dist/js/main.min.aa69868b.js
+++ Challenges/02 Junior/advice-generator-app/dist/js/main.min.aa69868b.js
@@ -1,3 +1,19 @@
-console.log("Script loaded");
+const adviceID = document.querySelector('[data-js="adviceID"]');
+const adviceText = document.querySelector('[data-js="adviceText"]');
+const adviceBtn = document.querySelector('[data-js="adviceBtn"]');
+const generateAdvice = async function() {
+    try {
+        const res = await fetch("https://api.adviceslip.com/advice");
+        const adviceJson = await res.json();
+        const { id, advice } = adviceJson.slip;
+        adviceID.textContent = id;
+        adviceText.textContent = advice;
+    } catch (error) {
+        adviceID.textContent = "---";
+        adviceText.textContent = "Sorry, We can't display the advice. Please try later.";
+    }
+};
+generateAdvice();
+adviceBtn.addEventListener("click", generateAdvice);
 
 //# sourceMappingURL=main.min.aa69868b.js.map
--- Challenges/02 Junior/advice-generator-app/src/js/main.js
+++ Challenges/02 Junior/advice-generator-app/src/js/main.js
@@ -1,1 +1,21 @@
-console.log("Script loaded");
+const adviceID = document.querySelector('[data-js="adviceID"]');
+const adviceText = document.querySelector('[data-js="adviceText"]');
+const adviceBtn = document.querySelector('[data-js="adviceBtn"]');
+
+const generateAdvice = async function () {
+  try {
+    const res = await fetch('https://api.adviceslip.com/advice');
+    const adviceJson = await res.json();
+    const { id, advice } = adviceJson.slip;
+
+    adviceID.textContent = id;
+    adviceText.textContent = advice;
+  } catch (error) {
+    adviceID.textContent = '---';
+    adviceText.textContent =
+      "Sorry, We can't display the advice. Please try later.";
+  }
+};
+
+generateAdvice();
+adviceBtn.addEventListener('click', generateAdvice);
