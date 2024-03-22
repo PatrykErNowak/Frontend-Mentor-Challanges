ae5e60d8433d1e618ba02fb066d9555447aeab57
Patryk Nowak <patryk.ernest.nowak@gmail.com>
2024-03-22 14:22:10 +0100

    Calculator app: Delete unused files & build app for production

--- Challenges/03 Intermediate/calculator-app/dist/js/index-legacy-BJY37hrn.js
+++
@@ -1,1 +0,0 @@
-System.register([],(function(e,t){"use strict";return{execute:function(){var e=document.createElement("style");e.textContent="body{background-color:#639}\n",document.head.appendChild(e),console.log("Hello")}}}));
