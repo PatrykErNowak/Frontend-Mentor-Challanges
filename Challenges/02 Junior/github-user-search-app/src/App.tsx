5a0c753cc75a106937a0ad5619584225ff4a3a36
Patryk Nowak <patryk.ernest.nowak@gmail.com>
2024-07-02 16:59:09 +0200

    Github search app: Init commit & configure global styles

---
+++ Challenges/02 Junior/github-user-search-app/src/App.tsx
@@ -0,0 +1,12 @@
+import GlobalStyles from './styles/GlobalStyles';
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <div>github</div>
+    </>
+  );
+}
+
+export default App;
---
+++ Challenges/02 Junior/github-user-search-app/src/styles/GlobalStyles.ts
@@ -0,0 +1,95 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyles = createGlobalStyle`
+:root {
+  --primary-color: #0079FF;
+  --primary-hover-color: #60ABFF;
+  --error-color: #F74646;
+
+  &.light-mode {
+    --heading-color: #2B3442;
+    --text-color: rgba(75, 106, 155, 0.75);
+    --strong-text-color: #697C9A;
+    
+    --bg-color: #F6F8FF;
+
+    --main-container-color: #FEFEFE;
+    --child-container-color: #F6F8FF;
+  }
+  &.dark-mode {
+    --heading-color: #FFFFFF;
+    --text-color: rgba(255,255,255,0.75);
+    --strong-text-color: #FFFFFF;
+
+    --bg-color: #141D2F;
+
+    --main-container-color: #1E2A47;
+    --child-container-color: #141D2F;
+  }
+}
+
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+
+  transition: background-color 0.3s, border 0.3s;
+}
+
+html {
+  font-size: 62.5%;
+}
+
+body {
+  font-family: "Space Mono", monospace;
+  font-size: 1.5rem;
+  line-height: 2.5rem;
+  color: var(--text-color);
+  background-color: var(--bg-color);
+}
+
+input,
+button,
+textarea,
+select {
+  font: inherit;
+  color: inherit;
+}
+
+button {
+  cursor: pointer;
+}
+
+button:has(svg) {
+  line-height: 0;
+}
+
+a {
+  color: inherit;
+  text-decoration: none;
+}
+
+ul {
+  list-style: none;
+}
+
+p,
+h1,
+h2,
+h3,
+h4,
+h5,
+h6 {
+  overflow-wrap: break-word;
+  hyphens: auto;
+}
+
+img {
+  max-width: 100%;
+}
+`;
+
+export default GlobalStyles;
